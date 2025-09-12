d7a5e61e64e2207d7282680c1207fdfd119e3745
Rainer Stropek <rainer@timecockpit.com>
2025-09-12 10:24:51 +0200

    Saved in TypeScript Playground at 2025-09-12T08:24:50.997Z

---
+++ house/index.ts
@@ -0,0 +1,27 @@
+function setup() {
+    createCanvas(2000, 5000);
+    background("cornflowerblue");
+
+    stroke("black")
+    strokeWeight(10)
+    fill("chocolate")
+    triangle(400, 200, 100, 350, 700, 350);
+
+    fill("red");
+    rect(200, 350, 400, 500);
+
+    fill("yellow");
+    rect(350, 600, 100, 250);
+
+    fill("chocolate");
+    stroke("chocolate");
+    strokeWeight(10)
+    rect(1100, 500, 100, 350)
+    
+    fill("green");
+    stroke("green")
+    circle(1050, 400, 350)
+    circle(1150, 250, 350)
+    circle(1250, 400, 350)
+
+}
