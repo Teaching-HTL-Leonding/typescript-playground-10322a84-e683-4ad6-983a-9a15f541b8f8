291a268c901c95d691d83d98a2218c7f4e747869
Rainer Stropek <rainer@timecockpit.com>
2025-09-19 10:42:47 +0200

    Saved in TypeScript Playground at 2025-09-19T08:42:46.545Z

---
+++ robot/index.ts
@@ -0,0 +1,20 @@
+function setup() {
+    createCanvas(1000, 1000);
+    background("Gainsboro");
+
+    stroke("black");
+    fill("white");
+    circle(500, 200, 20);
+
+    fill("grey");
+    line(500, 210, 500, 300);
+    rect(200, 300, 600, 600);
+
+    fill("black");
+    circle(300, 450, 100);
+    circle(700, 450, 100);
+
+    stroke("black");
+    fill("white");
+    rect(350, 650, 300, 100);
+}
