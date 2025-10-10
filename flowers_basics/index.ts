2e4b4b4e0792d8b68b7f76af11cf136af965bfd5
Rainer Stropek <rainer@timecockpit.com>
2025-10-10 09:41:39 +0200

    Saved in TypeScript Playground at 2025-10-10T07:41:38.652Z

--- flowers_basics/index.ts
+++ flowers_basics/index.ts
@@ -18,14 +18,27 @@ function setup() {
   circle(350, 100, DIAMETER)
   circle(300, 150, DIAMETER)
   circle(350, 200, DIAMETER)
-
-
   fill("yellow")
   stroke("yellow")
   circle(350, 150, 65)
 
-  circle(100, 150, 65)
+
+  
+  fill("lightgreen")
+  stroke("lightgreen")
+  circle(125, 100, DIAMETER2)
   circle(150, 150, DIAMETER2)
+  circle(75, 100, DIAMETER2)
+  circle(55, 160, DIAMETER2)
+  circle(110, 190, DIAMETER2)
+  fill("yellow")
+  stroke("yellow")
+  circle(100, 150, 65)
+
+  noFill()
+  stroke("green")
+  strokeWeight(20)
+  arc(-75, 300, 500, 500, -25, 45)
 
 
 }
