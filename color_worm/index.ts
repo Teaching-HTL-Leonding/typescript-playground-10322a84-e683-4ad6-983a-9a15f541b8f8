9297a2995e2e6f9552a645361e615dbb423c9f6f
Rainer Stropek <rainer@timecockpit.com>
2025-10-24 09:07:37 +0200

    Saved in TypeScript Playground at 2025-10-24T07:07:37.245Z

--- color_worm/index.ts
+++ color_worm/index.ts
@@ -10,7 +10,7 @@ let color_hue: number = 0
 function mouseMoved() {
     fill(color_hue, 100, 100);
     circle(mouseX, mouseY, 100)
-    color_hue = color_hue + 5
+    color_hue = color_hue + 3
     color_hue = color_hue % 360
     
 }
