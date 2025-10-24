387d3f4f393474affddf2767745db5145082670b
Rainer Stropek <rainer@timecockpit.com>
2025-10-24 10:31:27 +0200

    Saved in TypeScript Playground at 2025-10-24T08:31:27.385Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -15,6 +15,13 @@ function mouseClicked() {
     let c = sqrt(a * a + b * b)
     let diameter = 2 * c
     circle(mouseX, mouseY, diameter);
+
+    line(mouseX, mouseY, lastClickX, lastClickY)
+    line(mouseX, mouseY, lastClickX, mouseY)
+    line(lastClickX, mouseY, lastClickX, lastClickY)
+
+
+
     lastClickX = mouseX
     lastClickY = mouseY
 }
