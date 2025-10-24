27253459d12fd8eb7d9cdc92ad10d6c93d8caf58
Rainer Stropek <rainer@timecockpit.com>
2025-10-24 08:52:16 +0200

    Saved in TypeScript Playground at 2025-10-24T06:52:16.004Z

--- color_worm/index.ts
+++ color_worm/index.ts
@@ -1,13 +1,16 @@
 function setup() {
-    createCanvas(500, 500)
+    createCanvas(1000, 1000)
     colorMode(HSB)
     background(0, 100, 0)
     noStroke();
 }
 
+let color_hue: number = 0
+
 function mouseMoved() {
     fill(color_hue, 100, 100);
-    circle(mouseX, mouseY, 40)
-    color_hue = color_hue + 1
+    circle(mouseX, mouseY, 100)
+    color_hue = color_hue + 5
+    color_hue = color_hue % 360
     
 }
