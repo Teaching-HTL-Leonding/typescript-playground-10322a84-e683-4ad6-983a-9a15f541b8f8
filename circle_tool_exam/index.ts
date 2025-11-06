4b15c8920e78fdf22b6e029658918fb8d6f161ed
Rainer Stropek <rainer@timecockpit.com>
2025-11-06 13:33:31 +0100

    Saved in TypeScript Playground at 2025-11-06T12:33:30.833Z

--- circle_tool_exam/index.ts
+++ circle_tool_exam/index.ts
@@ -13,6 +13,8 @@ function mouseMoved() {
     let w = (mouseX-width/2) * 2
     let h = (mouseY-height/2) * 2
     ellipse(width/2, height/2, w, h);
+    line(width/2 - w/2, height/2, width/2 + w/2, height/2)
+    line()
 
     let a = w/2 * h/2  * Math.PI;
 
