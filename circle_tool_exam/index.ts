0602f49a112357243c9943f65c1c564d78c311eb
Rainer Stropek <rainer@timecockpit.com>
2025-11-06 13:18:32 +0100

    Saved in TypeScript Playground at 2025-11-06T12:18:31.863Z

---
+++ circle_tool_exam/index.ts
@@ -0,0 +1,24 @@
+function setup() {
+    createCanvas(400, 200);
+    background("black");
+    angleMode(DEGREES);
+}
+
+function mouseMoved() {
+    background("black");
+    stroke("white");
+    strokeWeight(3);
+    noFill();
+
+    let w = (mouseX-width/2) * 2
+    let h = (mouseY-height/2) * 2
+
+    ellipse(width/2, height/2, w, h);
+
+    let a = w/2 * h/2  * Math.PI;
+
+    noStroke()
+    fill("white")
+    text(`Area=${a}`, 10, 190)
+
+}
