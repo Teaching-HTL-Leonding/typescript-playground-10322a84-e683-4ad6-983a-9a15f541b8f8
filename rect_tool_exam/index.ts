6420c0a21b4f90ac0225e1101135fa50112318a9
Rainer Stropek <rainer@timecockpit.com>
2025-11-07 08:20:49 +0100

    Saved in TypeScript Playground at 2025-11-07T07:20:49.457Z

---
+++ rect_tool_exam/index.ts
@@ -0,0 +1,20 @@
+function setup() {
+    createCanvas(400, 200);
+    background("black");
+    angleMode(DEGREES);
+}
+
+function mouseMoved() {
+    background("black");
+    noFill();
+    stroke("white")
+    strokeWeight(3)
+
+    rectMode(CENTER)
+    let w = (width/2 - mouseX) * 2
+    let h = (height/2 - mouseY) * 2
+    rect(width/2, height/2, w, h)
+
+    
+    text(`Area=${}`)
+}
