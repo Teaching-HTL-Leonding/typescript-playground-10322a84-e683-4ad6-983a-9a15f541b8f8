e438a2ef0bc7fb3b85d5d87c0a0b967257a31c80
Rainer Stropek <rainer@timecockpit.com>
2025-11-21 08:42:39 +0100

    Saved in TypeScript Playground at 2025-11-21T07:42:39.393Z

---
+++ color_quadrants/index.ts
@@ -0,0 +1,45 @@
+function setup() {
+    createCanvas(400, 200);
+    background("black");
+    noStroke();
+
+    fill("yellow")
+    rect(0, 0, width / 2, height / 2)
+    fill("green")
+    rect(width / 2, 0, width / 2, height / 2)
+    fill("blue")
+    rect(0, height / 2, width / 2, height / 2)
+    fill("red")
+    rect(width / 2, height / 2, width / 2, height / 2)
+
+    rectMode(CENTER)
+    fill("black")
+    rect(width / 2, height / 2, 170, 50)
+
+}
+
+function mouseClicked() {
+    fill("black")
+    rect(width / 2, height / 2, 170, 50)
+
+    noStroke()
+    fill("white")
+    textSize(30)
+    textAlign(CENTER, CENTER)
+
+    let message: string
+    if (mouseX <) {
+
+    }
+
+
+
+
+
+
+
+
+
+
+    text(message, width / 2, height / 2)
+}
