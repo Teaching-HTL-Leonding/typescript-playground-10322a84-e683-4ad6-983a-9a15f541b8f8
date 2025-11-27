e085af3ca37ca6688db19cdee67f2a82e30a1333
Rainer Stropek <rainer@timecockpit.com>
2025-11-27 13:37:04 +0100

    Saved in TypeScript Playground at 2025-11-27T12:37:04.176Z

--- bouncing_ball/index.ts
+++ bouncing_ball/index.ts
@@ -3,11 +3,17 @@ function setup() {
 }
 
 const circleDiameter = 50;
+const circleDiameter2 = 50;
 
-let circleCenterX = 200;
+let circleCenterX = 150;
 let circleCenterY = 0;
 let directionX = 2;
 let directionY = 2;
+
+let circleCenterX2 = 150;
+let circleCenterY2 = 300;
+let directionX2 = 2;
+let directionY2 = 2;
 
 function draw() {
   background("gold");
@@ -21,10 +27,31 @@ function draw() {
   circleCenterY += directionY;
 
   if (circleCenterY >= height || circleCenterY <= 0) {
-    directionY *= -0.8; 
+    directionY *= -1;
   }
 
   if (circleCenterX >= width || circleCenterX <= 0) {
-    directionX *= -0.8;
+    directionX *= -1;
   }
+
+
+
+
+
+  stroke("white");
+  strokeWeight(3);
+  fill("aqua");
+  circle(circleCenterX2, circleCenterY2, circleDiameter2);
+
+  circleCenterX2 += directionX2;
+  circleCenterY2 += directionY2;
+
+  if (circleCenterY2 >= height || circleCenterY2 <= 0) {
+    directionY2 *= -1;
+  }
+
+  if (circleCenterX2 >= width || circleCenterX2 <= 0) {
+    directionX2 *= -1;
+  }
+
 }
