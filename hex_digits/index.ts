d25d44c2ec8d5959d9d132c3d12f3d56a931a304
Rainer Stropek <rainer@timecockpit.com>
2025-11-28 10:48:28 +0100

    Saved in TypeScript Playground at 2025-11-28T09:48:28.243Z

--- hex_digits/index.ts
+++ hex_digits/index.ts
@@ -8,34 +8,90 @@ function setup() {
   let digit1: string
   let digit2: string
   let digit3: string
-  let digit4: string
-  let digit5: string
-  let digit6: string
 
   if (number1 === 10) {
     digit1 = "A"
+  }
+
+  else if (number1 === 11) {
+    digit1 = "B"
+  }
+
+  else if (number1 === 12) {
+    digit1 = "C"
+  }
+
+  else if (number1 === 13) {
+    digit1 = "D"
+  }
+
+  else if (number1 === 14) {
+    digit1 = "E" 
+  }
+
+  else if (number1 === 15) {
+    digit1 = "F"
+  }
+  
+  else {
+    digit1 = `${number1}`
+  }
+
+  if (number2 === 10) {
+    digit2 = "A"
   }
 
   else if (number2 === 11) {
     digit2 = "B"
   }
 
+  else if (number2 === 12) {
+    digit2 = "C"
+  }
+
+  else if (number2 === 13) {
+    digit2 = "D"
+  }
+
+  else if (number2 === 14) {
+    digit2 = "E" 
+  }
+
+  else if (number2 === 15) {
+    digit2 = "F"
+  }
+  
+  else {
+    digit2 = `${number2}`
+  }
+
+  if (number3 === 10) {
+    digit2 = "A"
+  }
+
+  else if (number3 === 11) {
+    digit3 = "B"
+  }
+
   else if (number3 === 12) {
     digit3 = "C"
   }
 
-  else if (number4 === 13) {
-    digit4 = "D"
+  else if (number3 === 13) {
+    digit3 = "D"
   }
 
-  else if (number5 === 14) {
-    digit5 = "E" 
+  else if (number3 === 14) {
+    digit3 = "E" 
   }
 
-  else if (number6 === 15) {
-    digit6 = "F"
+  else if (number3 === 15) {
+    digit3 = "F"
   }
-
+  
+  else {
+    digit2 = `${number2}`
+  }
 
 
   textAlign(CENTER, CENTER);
@@ -53,12 +109,9 @@ function setup() {
 
   textSize(30)
   fill("yellow")
-  text(number6, 45, 100)
-  text(number5, 105, 100)
-  text(number4, 165, 100)
   text(number3, 45, 100)
-  text(number2, 105, 100)
-  text(number1, 165, 100)
+  text(digit2, 105, 100)
+  text(digit1, 165, 100)
 
 
   text(num, width / 2, height / 2);
