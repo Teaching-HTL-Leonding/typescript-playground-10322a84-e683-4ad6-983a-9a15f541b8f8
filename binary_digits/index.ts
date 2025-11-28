843e3eaa9da5e28d06b5148a8a6df0895a194382
Rainer Stropek <rainer@timecockpit.com>
2025-11-28 09:45:39 +0100

    Saved in TypeScript Playground at 2025-11-28T08:45:38.624Z

---
+++ binary_digits/index.ts
@@ -0,0 +1,38 @@
+function setup() {
+  const num = Math.floor(random(0, 64));
+
+  const number1 = num % 2
+  const number2 = floor(num / 10) % 2
+  const number3 = floor(num / 100) % 2
+  const number4 = floor(num / 1000) % 2
+  const number5 = floor(num / 10000) % 2
+  const number6 = floor(num / 100000) % 2
+
+  textAlign(CENTER, CENTER);
+  fill("yellow");
+  noStroke();
+  text(num, width / 2, height / 2);
+
+
+  createCanvas(600, 500)
+  stroke("yellow")
+  noFill()
+  rect(20, 60, 50, 100)
+  rect(80, 60, 50, 100)
+  rect(140, 60, 50, 100)
+  rect(200, 60, 50, 100)
+  rect(260, 60, 50, 100)
+  rect(320, 60, 50, 100)
+
+  textSize(30)
+  fill("yellow")
+  text(number6, 45, 100)
+  text(number5, 105, 100)
+  text(number4, 165, 100)
+  text(number3, 225, 100)
+  text(number2, 285, 100)
+  text(number1, 345, 100)
+
+
+  text(num, width / 2, height / 2);
+}
