fbb16ab4de2daade60ec8a22f4487fc8124e400c
Rainer Stropek <rainer@timecockpit.com>
2025-12-05 08:48:27 +0100

    Saved in TypeScript Playground at 2025-12-05T07:48:27.320Z

--- bouncing_diamond_exam/index.ts
+++ bouncing_diamond_exam/index.ts
@@ -6,16 +6,24 @@ function setup() {
 let directionX = 1
 let directionY = 1
 
+let diamondCenterX = 150
+let diamondCenterY = 0
+
+
 function draw() {
     background("gold");
     stroke("white")
     strokeWeight(3)
     noFill()
 
-    line(width/2, height/height, width/4 * 3, height/4)
-    line(width/2, height/height, width/4, height/4)
-    line(width/4, height/4, width/2, height/2)
-    line(width/2, height/2, width/4 * 3, height/4)
+
+    line(diamondCenterX, diamondCenterY, diamondCenterX + 75, diamondCenterY + 75)
+    line(diamondCenterX, diamondCenterY, diamondCenterX - 75, diamondCenterY + 75)
+    line(diamondCenterX - 75, diamondCenterY + 75, diamondCenterX, diamondCenterY + 150)
+    line(diamondCenterX, diamondCenterY + 150, diamondCenterX + 75, diamondCenterY + 75)
+
 
 
 }
+
+
