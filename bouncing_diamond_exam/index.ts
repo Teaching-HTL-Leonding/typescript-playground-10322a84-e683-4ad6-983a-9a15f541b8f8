2ae66a06b1cb36199093e27559d43a7daf40846f
Rainer Stropek <rainer@timecockpit.com>
2025-12-05 08:17:44 +0100

    Saved in TypeScript Playground at 2025-12-05T07:17:44.046Z

--- bouncing_diamond_exam/index.ts
+++ bouncing_diamond_exam/index.ts
@@ -1,7 +1,26 @@
 function setup() {
     createCanvas(300, 300);
+    rectMode(CENTER);
 }
+
+let directionX = 1
+let directionY = 1
+
+const rectHeight = 50
+const rectWidth = 50
+let rectCenterX = 0
+let rectCenterY = 0
 
 function draw() {
     background("gold");
+    stroke("white")
+    strokeWeight(3)
+    noFill()
+
+    rect(rectCenterX, rectCenterY, rectWidth, rectHeight)
+
+
+    if ()
+
+
 }
