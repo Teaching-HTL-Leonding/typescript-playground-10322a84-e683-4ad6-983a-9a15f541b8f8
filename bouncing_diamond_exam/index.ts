ae828c228a7f249661d7b3bd38fce17fdd682d5f
Rainer Stropek <rainer@timecockpit.com>
2025-12-05 09:47:03 +0100

    Saved in TypeScript Playground at 2025-12-05T08:47:02.800Z

--- bouncing_diamond_exam/index.ts
+++ bouncing_diamond_exam/index.ts
@@ -2,11 +2,13 @@ function setup() {
     createCanvas(300, 300);
 }
 
-let directionX = 1
-let directionY = 1
+let directionX = 2
+let directionY = 2
 
 let diamondCenterX = 150
 let diamondCenterY = 0
+
+let countdown = 10
 
 
 function draw() {
@@ -16,23 +18,38 @@ function draw() {
     noFill()
 
 
-    line(diamondCenterX, diamondCenterY, diamondCenterX + 75, diamondCenterY + 75)
-    line(diamondCenterX, diamondCenterY, diamondCenterX - 75, diamondCenterY + 75)
-    line(diamondCenterX - 75, diamondCenterY + 75, diamondCenterX, diamondCenterY + 150)
-    line(diamondCenterX, diamondCenterY + 150, diamondCenterX + 75, diamondCenterY + 75)
+    line(diamondCenterX, diamondCenterY, diamondCenterX + 50, diamondCenterY + 50)
+    line(diamondCenterX, diamondCenterY, diamondCenterX - 50, diamondCenterY + 50)
+    line(diamondCenterX - 50, diamondCenterY + 50, diamondCenterX, diamondCenterY + 100)
+    line(diamondCenterX, diamondCenterY + 100, diamondCenterX + 50, diamondCenterY + 50)
 
     diamondCenterX += directionX;
     diamondCenterY += directionY;
 
     if (diamondCenterX >= width || diamondCenterX <= 0) {
         directionX *= -1;
+        countdown += -1
     }
 
     if (diamondCenterY >= height || diamondCenterY <= 0) {
         directionY *= -1;
+        countdown += -1
+    }
+
+    if (countdown === 0) {
+        directionX *= 0
+        directionY *= 0
     }
 
 
+    //Text
+    noStroke();
+    fill("black");
+    textSize(30);
+    textStyle(BOLD);
+    textAlign(CENTER, CENTER);
+
+    text(countdown, diamondCenterX, diamondCenterY + 50)
 }
 
 
