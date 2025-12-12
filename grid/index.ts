a5377449effd5c22408734a362826b67394fc81f
Rainer Stropek <rainer@timecockpit.com>
2025-12-12 08:19:05 +0100

    Saved in TypeScript Playground at 2025-12-12T07:19:05.471Z

---
+++ grid/index.ts
@@ -0,0 +1,23 @@
+function setup() {
+    const SIZE = 400;
+    const GRID = 25;
+
+    createCanvas(SIZE, SIZE);
+    background("black");
+
+    strokeWeight(0.25);
+    stroke("lightgray");
+
+    let i = GRID;
+    while (i < SIZE) {
+        line(i, 0, i, SIZE);
+        i += GRID;           
+    }
+
+    // Part 2: Draw horizontal lines
+    let a = GRID
+    while (a < SIZE) {
+        line(0, a, SIZE, a)
+        a += GRID
+    }
+}
