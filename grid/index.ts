f8a3241eb6e1491c792d6b9af9417ea7a76b2aae
Rainer Stropek <rainer@timecockpit.com>
2025-12-12 08:26:11 +0100

    Saved in TypeScript Playground at 2025-12-12T07:26:11.538Z

--- grid/index.ts
+++ grid/index.ts
@@ -6,12 +6,18 @@ function setup() {
     background("black");
 
     strokeWeight(0.25);
-    stroke("lightgray");
+    stroke("lightgrey");
 
+    //Part 1: Vertical lines
     let i = GRID;
     while (i < SIZE) {
         line(i, 0, i, SIZE);
         i += GRID;           
+    }
+
+    //Part 1 mit For-Schleife
+    for (i = GRID; i < SIZE; i += GRID) {
+        line(i, 0, i, SIZE)
     }
 
     // Part 2: Draw horizontal lines
@@ -20,4 +26,11 @@ function setup() {
         line(0, a, SIZE, a)
         a += GRID
     }
+
+    //Part 2 mit For-Schleife
+    for (a = GRID; a < SIZE; a += GRID) {
+        line(0, a, SIZE, a)
+    }
+
+
 }
