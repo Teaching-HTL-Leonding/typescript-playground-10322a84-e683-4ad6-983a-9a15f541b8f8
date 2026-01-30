f3222844c15cc825b82e826b5fe5a9edea297e11
Rainer Stropek <rainer@timecockpit.com>
2026-01-30 09:14:44 +0100

    Saved in TypeScript Playground at 2026-01-30T08:14:44.368Z

--- parse_coordinates_2/index.ts
+++ parse_coordinates_2/index.ts
@@ -1,6 +1,6 @@
 // The following string represents coordinates (x,y) of points
 // that you have to connect with lines. Each line connects two consecutive points.
-const points = "200,100 300,150 300,250 200,300 100,250 100,150 200,100";
+const points = "200,100 300,150 300,250 200,300 100,250 100,150 200,100 ";
 
 function setup() {
     createCanvas(400, 400);
@@ -9,27 +9,33 @@ function setup() {
     stroke("black");
     strokeWeight(3);
 
-    let x = 0
-    let y = 0
-    let coordinates = ""
+    let x1 = 0 //x Kooridnate vom vorherigen Punkt
+    let y1 = 0 //y Koordinate vom vorherigen Punkt
+    let x2 = 0 //Hilfsvariable für aktuelle X Koordinate
+    let y2 = 0 //Hilfsvariable für aktuelle Y Koordinate
+    let coordinates = "" //Hilftsvariable für aktuellen Wert
+    let spaceCharsfound = 0 //Wie viele Leerzeichen haben wir schon gefunden?
 
     for (let i = 0; i < points.length; i++) {
-
-
-
-        if (points[i] === ",") {
-            x = parseInt(coordinates)
+        if(points[i] === ",") {
+            //Komma gefunden -> in coordinates steht x
+            x2 = parseInt(coordinates)
             coordinates = ""
         } else if (points[i] === " ") {
-            y = parseInt(coordinates)
-            line(x, y, 200, 100)
-            line(200, 100, 300, 150)
-            line(300, 150, 300, 250)
-            line(300, 250, 200, 300)
-            line(200, 300, 100, 250)
-            line(100, 250, 100, 150)
-            line(100, 150, 200, 100)
+            //Leerzeichen gefunden -> im coordinates steht y
+            y2 = parseInt(coordinates)
+
+            if(spaceCharsfound > 0) {
+            line(x1, y1, x2, y2)
+            }
+
+            spaceCharsfound++
+            x1 = x2 //Den aktuellen Punkt als vorherigen Punkt merken
+            y1 = y2
             coordinates = ""
+        } else {
+            // Es ist eine Ziffer
+            coordinates += points[i]
         }
     }
 }
