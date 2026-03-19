b145144fcd532bea37279a4ca31969fcb4056543
Rainer Stropek <rainer@timecockpit.com>
2026-03-19 13:26:17 +0100

    Saved in TypeScript Playground at 2026-03-19T12:26:16.704Z

---
+++ drawing_symbols_with_switch/index.ts
@@ -0,0 +1,53 @@
+function setup() {
+  createCanvas(200, 200);
+  background("lightgray");
+
+  const diceSize = 200; // size of the square (width and height)
+
+  const symbol = Math.floor(random(1, 7));
+
+  // draw black background
+  fill("black");
+  noStroke();
+  rect(0, 0, diceSize, diceSize);
+
+  const centerX = diceSize / 2;
+  const centerY = diceSize / 2;
+  const size = 80;
+
+
+  // TODO: Use a switch statement to draw different symbols based on the value of symbol
+  // 1: Square
+  // 2: Circle
+  // 3: Triangle
+  // 4: Star emoji ⭐
+  // 5: Heart emoji ❤️
+  // 6: Lucky charm emoji 🍀
+
+
+  let message: string
+  switch(symbol) {
+    case 1: message = "□"
+    break
+
+    case 2: message = "○"
+    break
+
+    case 3: message = "△"
+    break
+
+    case 4: message = "⭐"
+    break
+
+    case 5: message = "❤️"
+    break
+
+    case 6: message = "🍀"
+    break
+  }
+
+  fill("white")
+  textAlign(CENTER)
+  textSize(70)
+  text(message, width/2, height/2)
+}
