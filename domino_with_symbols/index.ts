32cfb1d562ee789cb5f4d94cf0c323a91c010264
Rainer Stropek <rainer@timecockpit.com>
2026-03-30 20:49:09 +0200

    Saved in TypeScript Playground at 2026-03-30T18:49:09.328Z

--- domino_with_symbols/index.ts
+++ domino_with_symbols/index.ts
@@ -1,10 +1,14 @@
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(400, 200);
   background("lightgray");
 
-  const diceSize = 200; // size of the square (width and height)
+  const s1 = floor(random(1, 7))
+  const s2 = floor(random(1, 7))
+  drawDomino(s1, s2)
+}
 
-  const symbol = floor(random(1, 7));
+function drawSymbol(symbol: number) {
+  const diceSize = 200; // size of the square (width and height)
 
   // draw black background
   fill("black");
@@ -62,4 +66,35 @@ function setup() {
       text("🍀", centerX, centerY);
       break;
   }
+}
+
+function drawDomino(s1: number, s2: number) {
+  const diceSize = 200
+  fill("black")
+  noStroke()
+
+  rect(0, 0, diceSize, diceSize)
+  rect(diceSize, 0, diceSize, diceSize)
+
+  //Erstes Quadrat
+  drawSymbol(s1)
+
+  //Zweites Quadrat
+  push()
+  translate(diceSize, 0)
+  drawSymbol(s2)
+  pop()
+
+  //Trennstrich Mitte
+  stroke("white")
+  strokeWeight(5)
+  line(200, 30, 200, 170)
+
+  //Rahmen
+  stroke("lightgrey")
+  strokeWeight(15)
+  line(0, 0, 400, 0)
+  line(400, 0, 400, 200)
+  line(400, 200, 0, 200)
+  line(0, 200, 0, 0)
 }
